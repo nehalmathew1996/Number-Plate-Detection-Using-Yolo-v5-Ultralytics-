27171c4411747ba49bea922fadf915d2ffa63724
Nehal Mathew Alex <82964954+nehalmathew1996@users.noreply.github.com>
2022-01-17 08:09:06 +0530

    Yolo v5 on Image

---
+++ image.py
@@ -0,0 +1,28 @@
+#pip3 install torch==1.8.2+cu102 torchvision==0.9.2+cu102 torchaudio===0.8.2 -f https://download.pytorch.org/whl/lts/1.8/torch_lts.html
+
+# Header Files
+import torch 
+# import matplotlib
+# matplotlib.use('TkAgg')
+import matplotlib
+import matplotlib.pyplot as plt
+matplotlib.use('TkAgg')
+import numpy as np
+import cv2
+import os
+
+model = torch.hub.load('ultralytics/yolov5', 'custom', path='C:/Users/nehal/Music/Thesis/1st Review Final/yolo_v5 on trained model/best.pt', force_reload=True)
+
+img = 'C:\\Users\\nehal\\Music\\Thesis\\1st Review Final\\yolo_v5 on trained model\\3f796298e3bd8b8b.jpg'
+
+results = model(img)
+
+results.print()
+
+#%matplotlib inline 
+img=np.squeeze(results.render())
+
+cv2.imshow('Image',img)
+key=cv2.waitKey(1)
+
+cv2.destroyAllWindows()
